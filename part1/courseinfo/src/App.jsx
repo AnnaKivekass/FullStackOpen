c238948783ba805946a7e0e76bc9368097613027
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-01-15 13:48:02 +0200

    2.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,8 +3,6 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
-  console.log('props value is', props)
-
   return (
     <p>
       {props.name} {props.exercises}
@@ -13,7 +11,6 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  console.log ('props', props)
   return (
     <div>
       {props.parts.map((part) => (
@@ -23,16 +20,25 @@ const Content = (props) => {
   )
 }
 
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  console.log('Total parts:', props.parts)
+  console.log('Total exercises:', total)
+
+  return <p><b>total of {total} exercises</b></p>
+}
+
 const Course = (props) => {
   const { course } = props
 
-  console.log('course.name:', course.name)
-  console.log('course.parts:', course.parts)
+  console.log('course:', course)
 
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -42,21 +48,10 @@ const App = () => {
     name: 'Half Stack application development',
     id: 1,
     parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 1,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 70,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
+      { name: 'Fundamentals of React', exercises: 1, id: 1 },
+      { name: 'Using props to pass data', exercises: 70, id: 2 },
+      { name: 'State of a component', exercises: 14, id: 3 },
+      { name: 'Redux', exercises: 11, id: 4 }
     ]
   }
 
