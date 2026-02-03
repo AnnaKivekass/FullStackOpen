177ecb07064579ba8c300289c889544eb5a528e6
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-02-03 15:52:16 +0200

    4.2

--- part4/index.js
+++ part4/index.js
@@ -16,20 +16,6 @@ mongoose
     console.error('error connecting to MongoDB:', error.message)
   })
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
