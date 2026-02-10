420a155adda0d326959014906b988a7ce373df40
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-02-10 15:14:33 +0200

    5.10

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -151,9 +151,12 @@ const App = () => {
         <BlogForm createBlog={createBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} handleLike={likeBlog} />
-      ))}
+     {[...blogs]
+  .sort((a, b) => b.likes - a.likes)
+  .map((blog) => (
+    <Blog key={blog.id} blog={blog} handleLike={likeBlog} />
+  ))}
+ 
     </div>
   )
 }
