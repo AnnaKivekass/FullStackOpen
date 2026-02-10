391a9d7695b894d815daa2c3356fba5e5eb7cd75
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-02-10 15:22:56 +0200

    5.11

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Blog = ({ blog, handleLike }) => {
+const Blog = ({ blog, handleLike, handleRemove, user }) => {
   const [visible, setVisible] = useState(false)
 
   const blogStyle = {
@@ -14,24 +14,36 @@ const Blog = ({ blog, handleLike }) => {
   const toggleVisibility = () => {
     setVisible(!visible)
   }
+  
+  const ownerUsername = blog.user?.username
+  const ownerId = blog.user?.id || blog.user?._id || blog.user
+  const currentUserId = user?.id || user?._id
+
+  const isOwner =
+    (ownerUsername && ownerUsername === user?.username) ||
+    (ownerId && currentUserId && ownerId === currentUserId)
 
   return (
     <div style={blogStyle} className="blog">
       <div>
         {blog.title} {blog.author}{' '}
-        <button onClick={toggleVisibility}>
-          {visible ? 'hide' : 'view'}
-        </button>
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
       </div>
 
       {visible && (
         <div className="blogDetails">
           <div>{blog.url}</div>
+
           <div>
             likes {blog.likes}{' '}
             <button onClick={() => handleLike(blog)}>like</button>
           </div>
+
           <div>{blog.user?.name}</div>
+
+          {isOwner && (
+            <button onClick={() => handleRemove(blog)}>remove</button>
+          )}
         </div>
       )}
     </div>
