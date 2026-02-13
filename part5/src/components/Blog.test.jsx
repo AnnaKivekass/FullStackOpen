f97e415695ea1c274bc0592fc35e08d2a12e92f1
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-02-13 10:48:24 +0200

    5.14

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author, but url and likes are hidden by default', () => {
+describe('<Blog />', () => {
   const blog = {
     title: "Things I Don’t Know as of 2018",
     author: 'Dan Abramov',
@@ -10,11 +11,27 @@ test('renders title and author, but url and likes are hidden by default', () => 
     user: { username: 'anna', name: 'Anna' },
   }
 
-  render(<Blog blog={blog} />)
+  test('renders title and author, but url and likes are hidden by default', () => {
+    render(<Blog blog={blog} />)
 
-  expect(screen.getByText(/Things I/)).toBeInTheDocument()
-  expect(screen.getByText(/Dan Abramov/)).toBeInTheDocument()
+    expect(screen.getByText(/Things I Don’t Know as of 2018/i)).toBeInTheDocument()
+    expect(screen.getByText(/Dan Abramov/i)).toBeInTheDocument()
 
-  expect(screen.queryByText(blog.url)).toBeNull()
-  expect(screen.queryByText(/likes/i)).toBeNull()
+    expect(screen.queryByText(blog.url)).toBeNull()
+    expect(screen.queryByText(/likes/i)).toBeNull()
+  })
+
+  test('url, likes ja käyttäjä näytetään kun view-nappia painetaan', async () => {
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByRole('button', { name: /view|show/i })
+    await user.click(viewButton)
+
+    expect(screen.getByText(blog.url)).toBeInTheDocument()
+
+    expect(screen.getByText(/likes\s*12/i)).toBeInTheDocument()
+
+    expect(screen.getByText(blog.user.name)).toBeInTheDocument()
+  })
 })
