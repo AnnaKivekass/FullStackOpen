475c60b0ca4491f3eaf5682f13014715e5a07527
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-02-16 12:22:10 +0200

    6.5

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,7 +4,7 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.' // 6.4
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -22,19 +22,24 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   switch (action.type) {
 
-    case 'VOTE':
+    case 'VOTE': {
       const id = action.payload
 
-      return state.map(anecdote =>
+      const updatedState = state.map(anecdote =>
         anecdote.id !== id
           ? anecdote
           : { ...anecdote, votes: anecdote.votes + 1 }
       )
 
-    case 'CREATE':
-      return state.concat(action.payload)
+      return updatedState.sort((a, b) => b.votes - a.votes)
+    }
 
-    default:
+    case 'CREATE': {
+      const newState = state.concat(action.payload)
+      return newState.sort((a, b) => b.votes - a.votes)
+    }
+
+    default: 
       return state
   }
 }
