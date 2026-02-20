f119692edcde4a7d9542e766ad56acede6703996
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-02-20 13:00:05 +0200

    7.4

--- part7/src/components/CreateNew.jsx
+++ part7/src/components/CreateNew.jsx
@@ -1,24 +1,30 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from "../hooks"
 
 const CreateNew = ({ addNew }) => {
   const navigate = useNavigate()
 
-  const [content, setContent] = useState("")
-  const [author, setAuthor] = useState("")
-  const [info, setInfo] = useState("")
+  const content = useField("text")
+  const author = useField("text")
+  const info = useField("text")
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
 
     navigate("/")
+  }
+
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -28,23 +34,25 @@ const CreateNew = ({ addNew }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input value={content} onChange={e => setContent(e.target.value)} />
+          <input type={content.type} value={content.value} onChange={content.onChange} />
         </div>
 
         <div>
           author
-          <input value={author} onChange={e => setAuthor(e.target.value)} />
+          <input type={author.type} value={author.value} onChange={author.onChange} />
         </div>
 
         <div>
           url for more info
-          <input value={info} onChange={e => setInfo(e.target.value)} />
+          <input type={info.type} value={info.value} onChange={info.onChange} />
         </div>
 
         <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
 }
 
 export default CreateNew
+
